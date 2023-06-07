4e03be69397ca6469ed2e4875c75ab0e3a37581e
Priti44662 <135731060+Priti44662@users.noreply.github.com>
2023-06-07 10:02:39 +0530

    Add files via upload

---
+++ AreaOfRectangle2.java
@@ -0,0 +1,10 @@
+class AreaOfRectangle2
+ {
+   public static void main (String[] args)
+   {
+	   double length = 4.5;
+	   double width = 8.0;
+	   double area = length*width;
+	   System.out.println("Area of Rectangle is:"+area);
+   }
+}
